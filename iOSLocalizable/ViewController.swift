79a3dc703a5daeee9a187336f992fbfcfccc5735
thinhle <thinhlefrs@gmail.com>
2020-03-01 20:26:49 +0700

    Initial Commit

---
+++ iOSLocalizable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOSLocalizable
+//
+//  Created by ThinhLe on 3/1/20.
+//  Copyright © 2020 ThinhLe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
